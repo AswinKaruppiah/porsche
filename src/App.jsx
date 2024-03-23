b7a27475064cc8ca62e4fa0a718061a9d2c0a443
Aswin <89991183+AswinKaruppiah@users.noreply.github.com>
2024-03-23 16:53:59 +0530

    laptop view

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "./App.css";
 import Home from "./UI/Home";
@@ -7,10 +7,26 @@ import Upperlayer from "./UI/Upperlayer";
 import Video from "./UI/Video";
 
 function App() {
-  return (
-    <>
-      <Upperlayer />
-    </>
+  const [laptop, setlaptop] = useState();
+  useEffect(() => {
+    if (window.innerWidth >= 900) return setlaptop(true);
+    else return setlaptop(false);
+  }, []);
+
+  function widthResizer() {
+    if (window.innerWidth <= 900) return setlaptop(false);
+    else return setlaptop(true);
+  }
+  window.addEventListener("resize", widthResizer);
+  return laptop ? (
+    <Upperlayer />
+  ) : (
+    <div
+      className="h-screen font-porsche flex justify-center
+     items-center text-xl text-center"
+    >
+      <h1> View on the desktop for a better experience</h1>
+    </div>
   );
 }
 
